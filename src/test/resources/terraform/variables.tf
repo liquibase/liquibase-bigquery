af7cf97d9ad0380ac78fc39eea4e3a842d107916
Sayali M <sayali@Sayalis-MacBook-Pro>
2024-01-25 15:42:36 -0600

    set dafeult values for project_id and deployer_sa

--- src/test/resources/terraform/variables.tf
+++ src/test/resources/terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "GCP Project id"
+  default     = "testharnesstests"
   type        = string
 }
 
 variable "deployer_sa" {
   description = "Deployer service account"
+  default     = "gh-bq-deployer"
   type        = string
 }
