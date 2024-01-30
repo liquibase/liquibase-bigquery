bf63003e395644430d36995ed0ab8d64897ac76e
Sayali M <sayali@Sayalis-MacBook-Pro>
2024-01-30 15:34:36 -0600

    tf debug

--- src/test/resources/terraform/backend.tf
+++ src/test/resources/terraform/backend.tf
@@ -1,10 +1,21 @@
-
-terraform {
-  backend "remote" {
-    organization = "liquibase"
-    hostname     = "spacelift.io"
-    workspaces {
-      name = "liquibase-bigquery-testharness-tests"
-    }
-  }
-}
+#
+# terraform {
+#   backend "remote" {
+#     organization = "liquibase"
+#     hostname     = "spacelift.io"
+#     workspaces {
+#       name = "liquibase-bigquery-testharness-tests"
+#     }
+#   }
+# }
+# data "terraform_remote_state" "liquibase-bigquery" {
+#   backend = "remote"
+#
+#   config = {
+#     hostname     = "spacelift.io"
+#     organization = "liquibase"
+#     workspaces = {
+#       name = "liquibase-bigquery"
+#     }
+#   }
+# }
