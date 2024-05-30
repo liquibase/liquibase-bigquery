3191d086a0b84a3e1d302218111d1a930327c1df
Vitalii Makarchenkov <mail@vitaliimak.com>
2024-05-30 16:53:52 +0300

    Fix merge conflicts

--- src/test/java/liquibase/ext/bigquery/sqlgenerator/BigQueryAddForeignKeyConstraintGeneratorTest.java
+++ src/test/java/liquibase/ext/bigquery/sqlgenerator/BigQueryAddForeignKeyConstraintGeneratorTest.java
@@ -36,6 +36,6 @@ class BigQueryAddForeignKeyConstraintGeneratorTest {
         Sql[] sql = generator.generateSql(statement, database, null);
         assertEquals(1, sql.length);
         assertEquals(";", sql[0].getEndDelimiter());
-        assertEquals("SELECT 1", sql[0].toSql());
+        assertEquals("ALTER TABLE baseTableSchemaName.baseTableName ADD CONSTRAINT constraintName FOREIGN KEY (`null`) REFERENCES referencedTableSchemaName.referencedTableName (`null`) NOT ENFORCED", sql[0].toSql());
     }
 }
