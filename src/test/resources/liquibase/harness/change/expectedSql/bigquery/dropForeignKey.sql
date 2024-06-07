06cc9a6a2acc319d0a74d691f1ff7430e10ac61a
Vitalii Makarchenkov <mail@vitaliimak.com>
2024-06-07 13:05:29 +0300

    Fix tests

--- src/test/resources/liquibase/harness/change/expectedSql/bigquery/dropForeignKey.sql
+++ src/test/resources/liquibase/harness/change/expectedSql/bigquery/dropForeignKey.sql
@@ -1,2 +1,3 @@
+ALTER TABLE harness_test_ds.authors ADD PRIMARY KEY (id) NOT ENFORCED
 ALTER TABLE harness_test_ds.posts ADD CONSTRAINT fk_posts_authors_test FOREIGN KEY (author_id) REFERENCES harness_test_ds.authors (id) NOT ENFORCED
 ALTER TABLE harness_test_ds.posts DROP CONSTRAINT fk_posts_authors_test
