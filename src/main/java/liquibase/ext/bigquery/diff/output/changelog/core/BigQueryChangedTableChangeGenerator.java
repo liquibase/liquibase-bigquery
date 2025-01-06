66d503f4e5372772f6e7e3789f336efac4900e52
Wesley Willard <wwillard@datical.com>
2025-01-06 08:42:51 -0600

    DAT-18828
    Rework getPriority to handle diffChangelog

--- src/main/java/liquibase/ext/bigquery/diff/output/changelog/core/BigQueryChangedTableChangeGenerator.java
+++ src/main/java/liquibase/ext/bigquery/diff/output/changelog/core/BigQueryChangedTableChangeGenerator.java
@@ -18,6 +18,9 @@ public class BigQueryChangedTableChangeGenerator extends ChangedTableChangeGener
     @Override
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
         int priority = super.getPriority(objectType, database);
+        if (priority == PRIORITY_NONE) {
+            return priority;
+        }
         if (database instanceof BigQueryDatabase) {
             priority += PRIORITY_DATABASE;
         }
