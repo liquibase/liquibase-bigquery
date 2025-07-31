97a91fe2da5799f7913e8b8ca3a8cd705e8c1d6f
Alejandro Alvarez <github@jandroav.net>
2025-07-31 09:47:23 +0200

    Add credentials file to Google providers in Terraform test

--- src/test/resources/terraform/provider-impersonation.tf
+++ src/test/resources/terraform/provider-impersonation.tf
@@ -1,8 +1,9 @@
 provider "google" {
   impersonate_service_account = "${var.deployer_sa}@${var.project_id}.iam.gserviceaccount.com"
+  credentials                 = file("/mnt/workspace/gcp.json")
 }
 
 provider "google-beta" {
   impersonate_service_account = "${var.deployer_sa}@${var.project_id}.iam.gserviceaccount.com"
+  credentials                 = file("/mnt/workspace/gcp.json")
 }
-
